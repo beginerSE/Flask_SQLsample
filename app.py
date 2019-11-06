e428358f769cc14be726b6dfd2b2f8a2051c7b44
零細SE <42573936+beginerSE@users.noreply.github.com>
2019-11-06 13:18:56 +0900

    余計な部分を修正

--- app.py
+++ app.py
@@ -9,24 +9,58 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # SQLAlchemyでデータベースに接続する
 db_uri = 'sqlite:///test.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+db = SQLAlchemy(app)
 
-db = SQLAlchemy(app) 
 
 # データベースの仕様をpythonのクラスで定義する
+class Sample_table(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    name_ = db.Column(db.Integer)
+    sex = db.Column(db.Text())
+    hobby = db.Column(db.Text())
 
-class order_table(db.Model): 
-    orderid = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    pub_date = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
-    goods_number = db.Column(db.Integer)
-    payment = db.Column(db.Text())
-    
-    def __init__(self, pub_date, goods_number, payment):
+    def __init__(self, pub_date, name, sex, hobby):
         self.pub_date = pub_date
-        self.goods_number = goods_number
-        self.payment = payment
+        self.name = name
+        self.sex = sex
+        self.hobby = hobby
+
+
+# データベースの作成(2回目からはエラーになるが例外処理で無視する)
 try:
-    db.create_all() 
+    db.create_all()
     print('ok')
 except:
     print('すでに作成済みです')
-    
+
+
+# トップ画面を表示
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/add_sql')
+def add_sql():
+
+    # 日付を取得
+    date = datetime.now()
+
+    # 入力フォームのデータを取得
+    name = request.args["name"]
+    sex = request.args["gender"]
+    hobby = request.args["hobby"]
+
+    # テーブルにデータを追加する
+    add_data = Sample_table(date, name, sex, hobby)
+    db.session.add(add_data)
+
+    # テーブルへの変更を保存する
+    db.session.commit()
+
+    return render_template('redirect.html')
+
+
+# テスト環境起動
+app.run(debug=True)
